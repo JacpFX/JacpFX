b33858f676b7eaa384cb7a534348c205bf8f8dbf
Claudio Rosati <Claudio.Rosati@esss.se>
2018-04-26 07:46:34 +0200

    Fixed Javadoc compilation error.

--- JACP.API/src/main/java/org/jacpfx/api/annotations/component/View.java
+++ JACP.API/src/main/java/org/jacpfx/api/annotations/component/View.java
@@ -30,6 +30,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.jacpfx.api.context.JacpContext;
 
 /**
  * Defines a component that returns a view node to be displayed in a perspective target.
